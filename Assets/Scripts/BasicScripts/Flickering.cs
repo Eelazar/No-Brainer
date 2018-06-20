3b3c95f19020ad6b455fcf496d27a777c08fab57
Maurice_S <maurice.matz@outlook.com>
2018-06-20 16:11:40 +0200

    Visual Changes
    Added some visual scripts and made the tutorial more beautiful

---
+++ Assets/Scripts/BasicScripts/Flickering.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickering : MonoBehaviour {
+
+    private Light thisLight;
+    private float amountOfFlickers, defaultIntensity;
+    private bool flickering;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        thisLight = gameObject.GetComponent<Light>();
+        defaultIntensity = thisLight.intensity;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!flickering)
+        {
+            StartCoroutine(Flicker());
+            Debug.Log("Start Flicker");
+        }
+	}
+    
+
+    IEnumerator Flicker()
+    {
+        flickering = true;
+        amountOfFlickers = Random.Range(3, 7);
+        yield return new WaitForSeconds(Random.Range(3, 15));
+
+        for(int i = 0; i < amountOfFlickers; i++)
+        {
+            thisLight.intensity = Random.Range(0, 4);
+            yield return new WaitForSeconds(Random.Range(0, 0.1F));
+            thisLight.intensity = defaultIntensity;
+        }
+
+        thisLight.intensity = defaultIntensity;
+        flickering = false;
+    }
+}
---
+++ Assets/Scripts/BrainTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainTransition : MonoBehaviour {
+
+    public float multiplier;
+    public string scene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Trigger()
+    {
+        Initiate.Fade(scene, Color.white, multiplier);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Trigger();
+    }
+}
