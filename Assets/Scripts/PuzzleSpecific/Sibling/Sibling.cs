5d0728b680a31c0756b2676f0eefea2b594d67d8
Gueynar <34599226+Gueynar@users.noreply.github.com>
2018-07-03 17:08:04 +0200

    Maurice did some stuff on my laptop

--- Assets/Scripts/PuzzleSpecific/Sibling/Sibling.cs
+++ Assets/Scripts/PuzzleSpecific/Sibling/Sibling.cs
@@ -10,13 +10,15 @@ public class Sibling : MonoBehaviour {
     private Vector3 distance;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        gameObject.transform.position = spawn;
+        distance.x = gameObject.transform.position.x - brother.transform.position.x;
+        distance.z = gameObject.transform.position.z - brother.transform.position.z;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        distance.x = gameObject.transform.position.x - brother.transform.position.x;
-        distance.z = gameObject.transform.position.z - brother.transform.position.z;
         Vector3 v = new Vector3(brother.transform.position.x + distance.x, gameObject.transform.position.y, brother.transform.position.z + distance.z);
         gameObject.transform.position = v;
         gameObject.transform.rotation = brother.transform.rotation;
@@ -27,13 +29,15 @@ public class Sibling : MonoBehaviour {
         //Respawn the player if he collides with a deadly object
         if (other.tag == "Deadly")
         {
-            Respawn();
             brother.GetComponent<PlayerScript>().ReSpawn();
+            Respawn();
         }
     }
 
     public void Respawn()
     {
         gameObject.transform.position = spawn;
+        distance.x = gameObject.transform.position.x - brother.transform.position.x;
+        distance.z = gameObject.transform.position.z - brother.transform.position.z;
     }
 }
