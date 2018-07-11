1153b20ec2c5dc7f5dbeb3ffa0fcaeff8e11ac7a
Maurice_S <maurice.matz@outlook.com>
2018-07-11 14:49:34 +0200

    new avoid player script

---
+++ Assets/Scripts/PuzzleSpecific/Isolation/AvoidPlayerStay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidPlayerStay : MonoBehaviour {
+
+    public float range;
+
+    private float distance;
+    private GameObject player;
+    private Vector3 origin;
+
+    // Use this for initialization
+    void Start ()
+    {
+        origin = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distance = Vector3.Distance(origin, player.transform.position);
+
+        Vector3 v = this.transform.position - player.transform.position;
+        v.Normalize();
+        v *= (range - distance);
+
+        this.transform.position = v;
+    }
+}
