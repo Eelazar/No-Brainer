99f739c9662a43b0c0692a0b35fa287bd32a5481
Maurice_S <maurice.matz@outlook.com>
2018-07-12 15:01:45 +0200

    Attraction can be permanent :hearts:

--- Assets/Scripts/PuzzleSpecific/Anger/Attraction.cs
+++ Assets/Scripts/PuzzleSpecific/Anger/Attraction.cs
@@ -7,9 +7,11 @@ public class Attraction : MonoBehaviour {
 
     public float attractionRange;
     public float attractionForce;
+    public bool stayAttracted;
 
     private Collider[] attractableObjects;
     private float distance;
+    private bool attracted;
 
 	// Use this for initialization
 	void Start ()
@@ -24,13 +26,23 @@ public class Attraction : MonoBehaviour {
         foreach (Collider c in attractableObjects)
         {
             distance = Vector3.Distance(this.transform.position, c.transform.position);
-            if(distance <= attractionRange)
+
+            if (attracted)
+            {
+                Vector3 direction = transform.position - c.transform.position;
+                if (c.GetComponent<Rigidbody>() != null)
+                {
+                    c.GetComponent<Rigidbody>().AddForce(direction * attractionForce);
+                }
+            }
+            else if (distance <= attractionRange && attracted == false)
             {
                 Vector3 direction = transform.position - c.transform.position;
                 if(c.GetComponent<Rigidbody>() != null)
                 {
                     c.GetComponent<Rigidbody>().AddForce(direction * attractionForce);
                 }
+                attracted = true;
             }
         }
         Profiler.EndSample();
