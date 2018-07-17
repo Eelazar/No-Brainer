c66e71bcd509fb83549dc795b728d6b88aee2e16
Maurice_S <maurice.matz@outlook.com>
2018-07-17 16:43:59 +0200

    Added min time to door triggers

--- Assets/Scripts/BasicScripts/SlidingDoorTrigger.cs
+++ Assets/Scripts/BasicScripts/SlidingDoorTrigger.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class SlidingDoorTrigger : MonoBehaviour {
 
     public GameObject door;
+    public float minTimePassed;
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && Time.time > minTimePassed)
         {
             door.GetComponent<SlidingDoor>().StartCoroutine(door.GetComponent<SlidingDoor>().Trigger());
         }
