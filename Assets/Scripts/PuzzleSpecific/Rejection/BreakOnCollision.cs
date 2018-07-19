1f9a5c5a48942db5656d4fa299cbde0222121eb3
Maurice_S <maurice.matz@outlook.com>
2018-07-19 15:14:12 +0200

    Added breakOnCollision script

---
+++ Assets/Scripts/PuzzleSpecific/Rejection/BreakOnCollision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakOnCollision : MonoBehaviour {
+
+    [Tooltip("Objects tagged with this tag will be able to break the object")]
+    public string tagThatBreaks;
+    [Tooltip("Amount of damage needed to fully break the object")]
+    public float totalDamage;
+    [Tooltip("Amount of damage dealt by one collision")]
+    public float damage;
+    [Header("Sounds")]
+    public AudioClip damageSound;
+    public AudioClip destroySound;
+
+
+    private AudioSource source;
+    private float damageCounter;
+    private bool destroyed;
+
+    
+	void Start ()
+    {
+        source = GetComponent<AudioSource>();
+	}
+	
+	void Update ()
+    {
+		if(damageCounter >= totalDamage && destroyed == false)
+        {
+            destroyed = true;
+            AudioSource.PlayClipAtPoint(destroySound, transform.position);
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == tagThatBreaks)
+        {
+            source.PlayOneShot(damageSound);
+            damageCounter += damage;
+        }
+    }
+}
