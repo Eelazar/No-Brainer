7f307e985a21443dfbc27145ecc01e4e447854fd
Maurice_S <maurice.matz@outlook.com>
2018-07-20 17:16:35 +0200

    Added save delete button to menu

---
+++ Assets/Scripts/DeletePrefs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeletePrefs : MonoBehaviour {
+    
+
+
+	void Start () {
+        this.GetComponent<Button>().onClick.AddListener(ResetPrefs);
+	}
+	
+	void ResetPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
