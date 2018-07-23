e3190b78cb6df2f59f8abdd3623836b6b2539993
Maurice <maurice.matz@outlook.com>
2018-07-23 23:05:49 +0200

    New Menu

--- Assets/Scripts/Menu&UI Stuff/MenuManager.cs
+++ Assets/Scripts/Menu&UI Stuff/MenuManager.cs
@@ -5,42 +5,82 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
-    
-    public Toggle devMode;
 
-    private Button[] menuButtons;
-    
-	void Start ()
+    [Header("References")]
+    [Tooltip("The object containing the main menu buttons")]
+    public GameObject mainMenuPage;
+    [Tooltip("The object containing the level menu buttons")]
+    public GameObject levelMenuPage;
+    [Space]
+    [Tooltip("The button that switches back to the main menu")]
+    public Button backButton;
+
+
+    //Private array for easy deactivating
+    private List<GameObject> menuPages;    
+
+
+    //Current Menu from list
+    public enum MenuPage { Main, Levels }
+    [HideInInspector]
+    public MenuPage currentMenu;
+
+    void Start ()
     {
-        menuButtons = this.GetComponentsInChildren<Button>();
-        for (int i = 0; i < menuButtons.Length; i++)
-        {
-            if (i == 0) menuButtons[i].gameObject.SetActive(true);
-            else menuButtons[i].gameObject.SetActive(false);
-        }
+        //Add pages to private array
+        menuPages = new List<GameObject>();
+        menuPages.Add(mainMenuPage);
+        menuPages.Add(levelMenuPage);
 
+        //Turn on the main menu
+        SwitchMenu(MenuPage.Main);
+
+        //Load the background scene
         SceneManager.LoadSceneAsync(PlayerPrefs.GetString("ScenePreview", "TutorialPreview"), LoadSceneMode.Additive);
+
+        //Arm the back button
+        backButton.onClick.AddListener(BackToMenu);
     }
 	
-	// Update is called once per frame
-	void Update ()
+
+    //Switches to a different menu
+    public void SwitchMenu(MenuPage newPage)
     {
-        if (devMode.isOn)
+        switch (newPage)
         {
-            for (int i = 0; i < menuButtons.Length; i++)
-            {
-                menuButtons[i].gameObject.SetActive(true);
-            }
+            case MenuPage.Main:
+
+                DeactivateAll();
+                mainMenuPage.SetActive(true);
+                currentMenu = MenuPage.Main;
+                break;
+
+            case MenuPage.Levels:
+
+                DeactivateAll();
+                levelMenuPage.SetActive(true);
+                backButton.gameObject.SetActive(true);
+                currentMenu = MenuPage.Levels;
+                break;
+
+            default:
+                break;
         }
-        else
+    }
+
+    //Deactivates all buttons, new menu pages have to be manually added
+    void DeactivateAll()
+    {
+        foreach(GameObject go in menuPages)
         {
-            for (int i = 0; i < menuButtons.Length; i++)
-            {
-                if (i == 0) menuButtons[i].gameObject.SetActive(true);
-                else menuButtons[i].gameObject.SetActive(false);
-            }
+            go.SetActive(false);
         }
 
+        backButton.gameObject.SetActive(false);
     }
-    
+
+    void BackToMenu()
+    {
+        SwitchMenu(MenuPage.Main);
+    }
 }
---
+++ Assets/Scripts/Menu&UI Stuff/MenuPageSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPageSwitch : MonoBehaviour {
+
+    public MenuManager.MenuPage newPage;
+
+	void Start ()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(SwitchToMenuPage);
+	}
+
+    void SwitchToMenuPage()
+    {
+        GameObject.Find("MenuManager").GetComponent<MenuManager>().SwitchMenu(newPage);
+    }
+}
