370cfd7d5805f90f6a82c31eb2c0c233b76dbdf4
Maurice_S <maurice.matz@outlook.com>
2018-07-23 14:44:35 +0200

    added sibling animation trigger

--- Assets/Scripts/PuzzleSpecific/Sibling/Sibling.cs
+++ Assets/Scripts/PuzzleSpecific/Sibling/Sibling.cs
@@ -8,10 +8,13 @@ public class Sibling : MonoBehaviour {
     public GameObject brother;
 
     private Vector3 distance;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start ()
     {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+
         gameObject.transform.position = spawn;
         distance.x = gameObject.transform.position.x - brother.transform.position.x;
         distance.z = gameObject.transform.position.z - brother.transform.position.z;
@@ -22,7 +25,16 @@ public class Sibling : MonoBehaviour {
         Vector3 v = new Vector3(brother.transform.position.x + distance.x, gameObject.transform.position.y, brother.transform.position.z + distance.z);
         gameObject.transform.position = v;
         gameObject.transform.rotation = brother.transform.rotation;
-	}
+
+        if (brother.GetComponent<PlayerScript>().walking)
+        {
+            animator.SetBool("moving", true);
+        }
+        else if (brother.GetComponent<PlayerScript>().walking == false)
+        {
+            animator.SetBool("moving", false);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
